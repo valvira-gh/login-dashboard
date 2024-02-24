355801814a7977fe7d79be9978d592b651dd9cb5
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-02-24 18:07:52 +0200

    Started Login component, updated users-table

--- app/api/create-users-table/route.ts
+++ app/api/create-users-table/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export const GET = async (request: Request) => {
     try {
         const result =
-            await sql`CREATE TABLE Users ( id SERIAL PRIMARY KEY, name VARCHAR(255) unique, email VARCHAR(255) unique, password VARCHAR(255) )`;
+            await sql`CREATE TABLE new_users ( id SERIAL PRIMARY KEY, name VARCHAR(255) unique, email VARCHAR(255) unique, password VARCHAR(255), isLogged BOOLEAN DEFAULT false )`;
 
         return NextResponse.json({ result }, { status: 200 });
     } catch (error) {
