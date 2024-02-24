7c9c8b7df2f20c79a0b36004203953aad1e45d5d
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-02-24 17:32:54 +0200

    Wrong export type...

--- app/api/create-users-table/route.ts
+++ app/api/create-users-table/route.ts
@@ -1,7 +1,7 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
-const GET = async (request: Request) => {
+export const GET = async (request: Request) => {
     try {
         const result =
             await sql`CREATE TABLE Users ( id SERIAL PRIMARY KEY, name VARCHAR(255) unique, email VARCHAR(255) unique, password VARCHAR(255) )`;
@@ -11,5 +11,3 @@ const GET = async (request: Request) => {
         return NextResponse.json({ error }, { status: 500 });
     }
 }
-
-export default GET;
