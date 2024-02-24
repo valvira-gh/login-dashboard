0d6e3a4985fba01ae301fe2131b5eced6d4fa5fd
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-02-24 17:23:09 +0200

    vercel-postgres database and api route for create users table

---
+++ app/api/create-users-table/route.ts
@@ -0,0 +1,13 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+const GET = async (request: Request) => {
+    try {
+        const result =
+            await sql`CREATE TABLE Users ( id SERIAL PRIMARY KEY, name VARCHAR(255) unique, email VARCHAR(255) unique, password VARCHAR(255) )`;
+
+        return NextResponse.json({ result }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 });
+    }
+}
