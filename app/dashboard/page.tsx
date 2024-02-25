864efa2cab1772fbc745e31cf472ab28af726e14
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-02-25 12:51:06 +0200

    hh

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,6 @@
 import styles from "./page.module.css";
 
-const dashboardLinks = [
-  { label: `User Settings`, url: `/dashboard/settings` },
-  { label: `Analytics`, url: `/dashboard/analytics` },
-  { label: `Billing`, url: `/dashboard/billing` },
-  { label: `Logout`, url: `/logout` },
-];
-
 const Dashboard: React.FC = () => {
-  //
-
   return (
     <section className={styles.dashboard}>
       <h3 className={styles.title}>Dashboard</h3>
