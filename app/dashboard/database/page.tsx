799776f5d7fe0ce35c8e59467d66194e2e2b7859
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-02-25 13:52:20 +0200

    reversed

--- app/dashboard/database/page.tsx
+++ app/dashboard/database/page.tsx
@@ -1,5 +1,22 @@
 import styles from "./page.module.css";
-import { FetchUsersBtn } from "./actions";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  const allUsers = await prisma.users.findMany();
+  console.log(allUsers);
+};
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
 const Database: React.FC = async () => {
   //
@@ -7,7 +24,6 @@ const Database: React.FC = async () => {
   return (
     <section className={styles.container}>
       <h3 className={styles.title}>Database</h3>
-      <FetchUsersBtn />
     </section>
   );
 };
